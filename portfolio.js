c8d0b10b7f2a5f1dc5adda7d190c63716598b63f
CamelIsTheBestConvention <ansdj3523@gmail.com>
2023-03-18 02:51:17 +0900

    making portfolio

---
+++ portfolio.js
@@ -0,0 +1,47 @@
+window.onload = function () {
+  console.log(document.getElementById("bigCircle").getAttribute("data-id"));
+
+  console.log(document.getElementById("bigCircle").getAttribute("class"));
+
+  document.documentElement.setAttribute("color-theme", "pink");
+  //최상위 root에 이 속성을 셋팅해준겁니다
+};
+
+let darkMode = document.getElementById("darkMode");
+
+darkMode.addEventListener("click", (event) => {
+  //e ===> 이벤트가 일어난 객체
+  //e.target ==> 이벤트가 일어난 객체를 선택
+  //e.target.innerText => 일어난 개체를 선택해서 innerText 속성을 선택
+
+  if (event.target.innerText === "RED") {
+    document.documentElement.setAttribute("color-theme", "dark");
+    event.target.innerText = "BLUE";
+  } else {
+    document.documentElement.setAttribute("color-theme", "pink");
+    event.target.innerText = "RED";
+  }
+});
+
+// 스크롤 내리기
+// function scrollDown() {
+//   window.scroll({
+//     top: 890,
+//     behavior: "smooth",
+//   });
+// }
+
+function scrollDown() {
+  window.scroll({
+    top: window.innerHeight,
+    behavior: "smooth",
+  });
+}
+
+window.addEventListener(
+  "wheel",
+  function (e) {
+    e.preventDefault();
+  },
+  { passive: false }
+);
