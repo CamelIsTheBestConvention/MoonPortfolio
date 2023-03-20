76914329c637121e07758b5428d70a390138c04a
CamelIsTheBestConvention <ansdj3523@gmail.com>
2023-03-20 12:35:31 +0900

    making skill part

--- portfolio.js
+++ portfolio.js
@@ -4,7 +4,7 @@ window.onload = function () {
   console.log(document.getElementById("bigCircle").getAttribute("class"));
 
   document.documentElement.setAttribute("color-theme", "pink");
-  //최상위 root에 이 속성을 셋팅해준겁니다
+  //최상위 root에 이 속성을 세팅
 };
 
 let darkMode = document.getElementById("darkMode");
@@ -31,6 +31,7 @@ darkMode.addEventListener("click", (event) => {
 //   });
 // }
 
+// about me 클릭시 내려가기
 function scrollDown() {
   window.scroll({
     top: window.innerHeight,
@@ -38,6 +39,7 @@ function scrollDown() {
   });
 }
 
+// 휠 이벤트 막기
 window.addEventListener(
   "wheel",
   function (e) {
@@ -45,3 +47,50 @@ window.addEventListener(
   },
   { passive: false }
 );
+
+// 모달창
+const modal = document.getElementById("modal");
+
+function modalOn() {
+  modal.style.display = "flex";
+}
+function isModalOn() {
+  return modal.style.display === "flex";
+}
+function modalOff() {
+  modal.style.display = "none";
+}
+
+const closeBtn = modal.querySelector(".close-area");
+closeBtn.addEventListener("click", (e) => {
+  modalOff();
+});
+
+modal.addEventListener("click", (e) => {
+  const evTarget = e.target;
+  if (evTarget.classList.contains("modal-overlay")) {
+    modalOff();
+  }
+});
+
+window.addEventListener("keyup", (e) => {
+  if (isModalOn() && e.key === "Escape") {
+    modalOff();
+  }
+});
+
+var modalContent1 = document.getElementById("modal-content1");
+var modalContent2 = document.getElementById("modal-content2");
+// 퍼블리싱 모달
+const markupImg = document.getElementById("markup-img");
+markupImg.addEventListener("click", (e) => {
+  modalContent1.innerHTML = "이건 퍼블리싱";
+  modalOn();
+});
+
+// 리액트 모달
+const reactImg = document.getElementById("react-img");
+reactImg.addEventListener("click", (e) => {
+  modalOn();
+  modalContent1.innerHTML("이건 리액트");
+});
